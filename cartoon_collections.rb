e11c095fc024396bdc0b32017815cf7299594946
SamHarron <harronsam@gmail.com>
2020-11-19 12:39:27 -0500

    commit tree cef955254e14998ba6d6284533adfde5344e1d78

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,56 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+puts roll_call_dwarves(dwarves)
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+
+
+def summon_captain_planet(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array << array[i].capitalize + "!"
+    i += 1
+  end
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+puts summon_captain_planet(planeteer_calls)
+puts " "
+
+
+assorted_words = ["two", "go", "I dont know the planeteers", "bop"]
+
+def long_planeteer_calls(array)
+  i = 0
+    if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i += 1
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+puts long_planeteer_calls(assorted_words)
+puts " "
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |x|
+    cheese_types.include?(x)
+  end
 end
+
+puts find_the_cheese(cheese_types)
+puts " "
